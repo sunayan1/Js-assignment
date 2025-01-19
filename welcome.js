d4c6dedc51293e810480d861b437cf47aee211ce
sunayan1 <sunayanduwal999@gmail.com>
2025-01-19 19:27:36 +0545

    welcome.js committed

---
+++ welcome.js
@@ -0,0 +1,17 @@
+//arrow function
+const welcome = (name)=>{
+    return `Welcome ${name}`;
+}
+console.log(welcome('sunayan'));
+
+//function expression
+const welcomeexpression = function(name){
+    return `Welcome ${name}`;
+}
+console.log(welcomeexpression('sunayan'));
+
+//functino declaration 
+function welcomedeclaration(name){
+    return `Welcome ${name}`;
+}
+console.log(welcomedeclaration('sunayan'));
