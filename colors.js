ed50e9a903310632d3e771b84eda179bbdaf0603
sunayan1 <sunayanduwal999@gmail.com>
2025-01-20 19:32:41 +0545

    colors.js

---
+++ colors.js
@@ -0,0 +1,12 @@
+//Given an array of colors const colors = ["red", "green", "blue"]; add yellow to this array, find the index of red, and console log the 2nd item.
+
+const colors = ["red", "green", "blue"];
+
+colors.push("yellow");
+
+console.log(colors.indexOf("red"));
+
+
+console.log(colors[2]);
+
+console.log(colors);
